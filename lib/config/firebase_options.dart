c9dbd4bb9bf5d2a423b51d7f5198516376fbf3cb
R Soares <r22777@outlook.com>
2026-05-04 01:17:16 -0300

    Y

--- lib/config/firebase_options.dart
+++ lib/config/firebase_options.dart
@@ -1,11 +1,3 @@
-// ATENÇÃO: Este arquivo é gerado automaticamente pelo Firebase CLI.
-//
-// Execute no terminal do seu projeto:
-//   flutterfire configure
-//
-// O comando acima substituirá este arquivo com as configurações
-// reais do seu projeto Firebase.
-
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
@@ -25,30 +17,29 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // TODO: substitua após rodar `flutterfire configure`
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
-    appId: 'YOUR_APP_ID',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  );
-
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
-    appId: 'YOUR_APP_ID',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    apiKey: 'AIzaSyBNox_MHUiLbtJKldUC5Wtezw3UfAuOJOM',
+    appId: '1:492117491640:android:3a0e63d78f085e34c3594d',
+    messagingSenderId: '492117491640',
+    projectId: 'skills-arena-c2c71',
+    storageBucket: 'skills-arena-c2c71.firebasestorage.app',
   );
 
+  // TODO: rode `flutterfire configure` para gerar as opções de iOS e Web
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
-    appId: 'YOUR_APP_ID',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    iosBundleId: 'com.example.skillsArena',
+    apiKey: 'YOUR_IOS_API_KEY',
+    appId: '1:492117491640:ios:f524e28eed161514c3594d',
+    messagingSenderId: '492117491640',
+    projectId: 'skills-arena-c2c71',
+    storageBucket: 'skills-arena-c2c71.firebasestorage.app',
+    iosBundleId: 'com.r27systems.skillsArena',
+  );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'YOUR_WEB_API_KEY',
+    appId: '1:492117491640:web:4e5c92d5980015bec3594d',
+    messagingSenderId: '492117491640',
+    projectId: 'skills-arena-c2c71',
+    storageBucket: 'skills-arena-c2c71.firebasestorage.app',
   );
 }
